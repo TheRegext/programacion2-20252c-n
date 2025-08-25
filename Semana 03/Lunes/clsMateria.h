0679d3dc6998114e2d2b6d1daa8b957b57598f85
MaximilianoWenner <88354878+MaximilianoWenner@users.noreply.github.com>
2025-08-25 20:29:44 -0300

    Add files via upload

---
+++ Semana 03/Lunes/clsMateria.h
@@ -0,0 +1,30 @@
+#ifndef CLSMATERIA_H_INCLUDED
+#define CLSMATERIA_H_INCLUDED
+
+class Materia{
+    ///PROPIEDADES:
+    ///INFORMACION QUE PRETENDO ALMACENAR DE MI OBJETO
+    private:
+        int codigo;
+        char nombre[50];
+        int cantidadAlumnos;
+        int cantidadDocentes;
+    public:
+    ///MÉTODOS (COMPORTAMIENTO):
+        void Cargar();
+        void Mostrar();
+        ///SETTERS
+        ///RECIBEN UN VALOR POR PARAMETRO Y SE LO ASIGNAN A
+        ///LA PROPIEDAD CORRESPONDIENTE.
+        void setCodigo(int c);
+        void setNombre(const char *n);
+        void setCantidadAlumnos(int cA);
+        void setCantidadDocentes(int cD);
+        ///GETTERS
+        int getCodigo();
+        const char *getNombre();
+        int getCantidadAlumnos();
+        int getCantidadDocentes();
+};
+
+#endif // CLSMATERIA_H_INCLUDED
